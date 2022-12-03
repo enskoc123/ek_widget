01fe6f0db2f84e39adbdc3cb133e3a3b511bbca7
enskoc123 <102908728+enskoc123@users.noreply.github.com>
2022-12-03 18:44:03 +0300

    03.12.2022
    yenilikler

--- lib/ek_widget.dart
+++ lib/ek_widget.dart
@@ -1,12 +1,21 @@
 library ek_widget;
-
+//Buton paketleri
 export 'package:ek_widget/buttons/button_vibrate.dart';
 export 'package:ek_widget/buttons/progressButton.dart';
 export 'package:ek_widget/buttons/dalga_effect_button.dart';
-export 'package:ek_widget/expand/expanded_widget.dart';
-export 'package:ek_widget/expand/expend_letter_widget.dart';
-export 'package:ek_widget/buttons/swich_button.dart';
-export 'package:ek_widget/buttons/swich_anim_button.dart';
+export 'package:ek_widget/buttons/dalgali_button2.dart';
 export 'package:ek_widget/buttons/gravity_button.dart';
 export 'package:ek_widget/buttons/manyetik_vutton.dart';
-export 'package:ek_widget/buttons/dalgali_button2.dart';
+export 'package:ek_widget/buttons/swich_anim_button.dart';
+export 'package:ek_widget/buttons/swich_button.dart';
+
+//Text expand paketleri
+export 'package:ek_widget/expand/expanded_widget.dart';
+export 'package:ek_widget/expand/expend_letter_widget.dart';
+
+//Login page paketleri
+export 'package:ek_widget/pages/fade_login.dart';
+export 'package:ek_widget/pages/login_page.dart';
+
+//Drawer paketlerş
+export 'package:ek_widget/drawer/draver1.dart';
